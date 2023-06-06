336e06624f695968e281e3ae350ed55251931a12
maximusbro <shidlovskiy.maksimka@gmail.com>
2023-06-06 16:39:35 -0600

    tabs change

--- resources/js/Components/InputSelect/index.tsx
+++ resources/js/Components/InputSelect/index.tsx
@@ -2,56 +2,56 @@ import React, { useRef, useEffect, memo, InputHTMLAttributes } from 'react'
 import InputError from '../InputError';
 
 export interface InputSelectProps extends InputHTMLAttributes<HTMLSelectElement> {
-  options: Array<string>;
-  name: string,
-  srcIcon?: string;
-  errors?: { [key: string]: string | undefined };
-  isFocused?: boolean;
+	options: Array<string>;
+	name: string,
+	srcIcon?: string;
+	errors?: { [key: string]: string | undefined };
+	isFocused?: boolean;
 }
 const InputSelect: React.FC<InputSelectProps> = memo(({
-  className,
-  name,
-  value,
-  errors,
-  title,
-  srcIcon,
-  options,
-  onChange,
-  onBlur,
-  isFocused
+	className,
+	name,
+	value,
+	errors,
+	title,
+	srcIcon,
+	options,
+	onChange,
+	onBlur,
+	isFocused
 }) => {
 
-  const localRef = useRef<HTMLInputElement>(null);
+	const localRef = useRef<HTMLInputElement>(null);
 
-  useEffect(() => {
-    if (isFocused) {
-      localRef.current?.focus();
-    }
-  }, []);
-  return (
-    <div className={className}>
-      <div className="form-inner mb-25">
-        <label htmlFor={name}>{title}</label>
-        <div className="input-area">
-          {srcIcon ? <img src={srcIcon} alt="" /> : null}
-          <select
-            className="nice-select"
-            onChange={onChange}
-            onBlur={onBlur}
-            value={value}
-            id={name}
-            name={name}>
-            {options.map((option, index) => {
-              return (
-                <option key={index} value={option}>{option}</option>
-              )
-            })}
-          </select>
-        </div>
-        {errors?.[name] ? <InputError message={errors[name]} /> : null}
-      </div>
-    </div>
-  )
+	useEffect(() => {
+		if (isFocused) {
+			localRef.current?.focus();
+		}
+	}, []);
+	return (
+		<div className={className}>
+			<div className="form-inner mb-25">
+				<label htmlFor={name}>{title}</label>
+				<div className="input-area">
+					{srcIcon ? <img src={srcIcon} alt="" /> : null}
+					<select
+						className="nice-select"
+						onChange={onChange}
+						onBlur={onBlur}
+						value={value}
+						id={name}
+						name={name}>
+						{options.map((option, index) => {
+							return (
+								<option key={index} value={option}>{option}</option>
+							)
+						})}
+					</select>
+				</div>
+				{errors?.[name] ? <InputError message={errors[name]} /> : null}
+			</div>
+		</div>
+	)
 })
 
 export default InputSelect
