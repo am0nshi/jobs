9294525a295f4dfd4ca7d080bcd15465de3d252c
maximusbro <shidlovskiy.maksimka@gmail.com>
2023-06-08 21:56:00 -0600

    End EditResume

---
+++ resources/js/Components/Profile/EditResume/BasicInformation/index.tsx
@@ -0,0 +1,193 @@
+import { FormEventHandler } from "react"
+import { useForm } from '@inertiajs/react';
+//Components
+import InputText from '@/Components/InputText';
+import InputSelect from '@/Components/InputSelect';
+import SocialNetwork from '../../MyProfile/SocialNetwork';
+import TextArea from '@/Components/TextArea';
+
+
+//Icons
+import UserIcon from "/public/assets/images/icon/user-2.svg";
+import CompanyIcon from "/public/assets/images/icon/company-2.svg";
+import DesignationIcon from "/public/assets/images/icon/designation-2.svg";
+import ExpirienceIcon from "/public/assets/images/icon/experiences-2.svg";
+import EmailIcon from "/public/assets/images/icon/email-2.svg";
+import QualificationIcon from "/public/assets/images/icon/qualification-2.svg";
+import PhoneIcon from "/public/assets/images/icon/phone-2.svg";
+import WebSiteIcon from "/public/assets/images/icon/website-2.svg";
+import LanguageIcon from "/public/assets/images/icon/language-2.svg";
+
+//options for InputSelect
+const qualificationOptions = {
+  "Bachelor Degree in CSE": "Bachelor Degree in CSE",
+  "IGCSE": "IGCSE",
+  "AS": "AS",
+  "A Level": "A Level",
+  "Matriculated": "Matriculated"
+};
+
+const BasicInformation = () => {
+  const { data, errors, setData, reset } = useForm({
+    firstName: "",
+    lastName: "",
+    desigation: "",
+    jobplace: "",
+    experiences: "",
+    qualification: "",
+    email: "",
+    phoneNumber: "",
+    website: "",
+    language: "",
+    description: "",
+  })
+
+  const onSubmit: FormEventHandler = (e) => {
+    e.preventDefault();
+    reset();
+  };
+
+  return (
+    <div className="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+      <form className="edit-profile-form profile-form  mb-60"
+        onSubmit={onSubmit}>
+        <div className="row">
+          <div className="col-md-6">
+            <InputText
+              onChange={e => setData('firstName', e.target.value)}
+              value={data.firstName}
+              errors={errors}
+              name="firstName"
+              placeholder='Mrs. Jacoline'
+              srcIcon={UserIcon}
+              title='First Name*'
+            />
+          </div>
+          <div className="col-xxl-4 col-md-6">
+            <InputText
+              onChange={e => setData('lastName', e.target.value)}
+              value={data.lastName}
+              errors={errors}
+              name="lastName"
+              placeholder='Frankly'
+              srcIcon={UserIcon}
+              title='Last Name*'
+            />
+          </div>
+          <div className="col-xxl-2 col-lg-12 position-relative">
+            <div className="drag-area">
+              <p>Upload Images</p>
+              <button type="button" className="upload-btn"><i className="bi bi-plus-lg"></i></button>
+              <input type="file" hidden />
+            </div>
+          </div>
+          <div className="col-md-6">
+            <InputText
+              onChange={e => setData('jobplace', e.target.value)}
+              value={data.jobplace}
+              errors={errors}
+              name="jobplace"
+              placeholder='Company Name'
+              srcIcon={CompanyIcon}
+              title='Company Name'
+            />
+          </div>
+          <div className="col-md-6">
+            <InputText
+              onChange={e => setData('desigation', e.target.value)}
+              value={data.desigation}
+              errors={errors}
+              name="desigation"
+              placeholder='UI/UX Engineer'
+              srcIcon={DesignationIcon}
+              title='Designation*'
+            />
+          </div>
+          <div className="col-md-6">
+            <InputText
+              onChange={e => setData('experiences', e.target.value)}
+              value={data.experiences}
+              errors={errors}
+              name="experiences"
+              placeholder='3.5 Years'
+              srcIcon={ExpirienceIcon}
+              title='Year of Experiences*'
+            />
+          </div>
+          <div className="col-md-6">
+            <InputSelect
+              onChange={e => setData('qualification', e.target.value)}
+              value={data.qualification}
+              errors={errors}
+              name="qualification"
+              placeholder='3.5 Years'
+              srcIcon={QualificationIcon}
+              options={qualificationOptions}
+              title='Qualification*'
+            />
+          </div>
+          <div className="col-md-6">
+            <InputText
+              onChange={e => setData('email', e.target.value)}
+              value={data.email}
+              errors={errors}
+              name="email"
+              placeholder='info@example.com'
+              srcIcon={EmailIcon}
+              title='Email*'
+            />
+          </div>
+          <div className="col-md-6">
+            <InputText
+              onChange={e => setData('phoneNumber', e.target.value)}
+              value={data.phoneNumber}
+              errors={errors}
+              name="phoneNumber"
+              placeholder='+880-17 *** *** **'
+              srcIcon={PhoneIcon}
+              title='Phone Number*'
+            />
+          </div>
+          <div className="col-md-6">
+            <InputText
+              onChange={e => setData('website', e.target.value)}
+              value={data.website}
+              errors={errors}
+              name="website"
+              placeholder='https://example.com'
+              srcIcon={WebSiteIcon}
+              title='Phone Number*'
+            />
+          </div>
+          <div className="col-md-6">
+            <InputText
+              onChange={e => setData('language', e.target.value)}
+              value={data.language}
+              errors={errors}
+              name="language"
+              placeholder='Bangla, English, Hindi'
+              srcIcon={LanguageIcon}
+              title='Language*'
+            />
+          </div>
+          <div className="col-md-12">
+            <TextArea
+              onChange={e => setData('description', e.target.value)}
+              value={data.description}
+              name="description"
+              placeholder='Something Write Yourself....'
+              title='Career Objective*' />
+          </div>
+          <div className="col-md-12">
+            <div className="form-inner">
+              <button className="primry-btn-2 lg-btn w-unset" type="submit">Update Resume</button>
+            </div>
+          </div>
+        </div>
+      </form>
+      <SocialNetwork />
+    </div>
+  )
+}
+
+export default BasicInformation
---
+++ resources/js/Components/Profile/EditResume/EditPersonalInfo/index.tsx
@@ -0,0 +1,131 @@
+import { FormEventHandler } from "react"
+import InputText from '@/Components/InputText'
+import { useForm } from '@inertiajs/react'
+//icons
+import CompanyIcon from "/public/assets/images/icon/company-2.svg"
+import InputSelect from '@/Components/InputSelect';
+import DesigationIcon from "/public/assets/images/icon/designation-2.svg"
+import CalendarIcon from "/public/assets/images/icon/calender2.svg"
+import InputCheckBox from '@/Components/InputCheckBox';
+import TextArea from '@/Components/TextArea';
+//Options for InputSelect
+const desigationOptions = {
+  "Back-end developer": "Back-end developer",
+  "Front-end developer": "Front-end developer",
+  "Full-stack developer": "Full-stack developer",
+};
+
+const EditPersonalInfo = () => {
+  const { data, errors, setData, reset } = useForm({
+
+    company: "",
+    desigation: "",
+    dateStart: "",
+    dateEnd: "",
+    isWorking: false,
+    responsibility: "",
+
+  })
+  const onSubmit: FormEventHandler = (e) => {
+    e.preventDefault();
+    reset();
+  };
+  return (
+    <div className="tab-pane active" id="professional" role="tabpanel" aria-labelledby="professional-tab">
+      <form
+        onSubmit={onSubmit}
+        className="edit-profile-form profile-form">
+        <div className="section-title2">
+          <h5>Employment Information:</h5>
+        </div>
+        <div className="experiences-row">
+          <div className="row">
+            <div className="col-lg-12">
+              <div className="info-title">
+                <h6>Add Your Experiences</h6>
+                <div className="dash"></div>
+              </div>
+            </div>
+            <div className="col-md-6">
+              <InputText
+                onChange={e => setData('company', e.target.value)}
+                value={data.company}
+                errors={errors}
+                name="company"
+                placeholder='Egenslab'
+                srcIcon={CompanyIcon}
+                title='Company Name*'
+              />
+            </div>
+            <div className="col-md-6">
+              <InputSelect
+                onChange={e => setData('desigation', e.target.value)}
+                value={data.desigation}
+                errors={errors}
+                name="desigation"
+                srcIcon={DesigationIcon}
+                options={desigationOptions}
+                title='Designation*'
+              />
+            </div>
+            <div className="col-md-6">
+              <InputText
+                onChange={e => setData('dateStart', e.target.value)}
+                value={data.dateStart}
+                errors={errors}
+                name="dateStart"
+                placeholder='DD/MM/YY'
+                srcIcon={CalendarIcon}
+                title='Starting Period*'
+              />
+            </div>
+            <div className="col-md-6">
+
+              <InputText
+                onChange={e => setData('dateEnd', e.target.value)}
+                value={data.dateEnd}
+                errors={errors}
+                name="dateEnd"
+                placeholder='DD/MM/YY'
+                srcIcon={CalendarIcon}
+                title='Ending Period*'
+              />
+            </div>
+
+            <div className="col-md-12">
+              <InputCheckBox
+                onChange={e => setData('isWorking', e.target.checked)}
+                value={data.isWorking}
+                name="isWorking"
+                placeholder='DD/MM/YY'
+                srcIcon={CalendarIcon}
+                text='Continuing Working Here*'
+              />
+            </div>
+            <div className="col-md-12">
+              <TextArea
+                onChange={e => setData('responsibility', e.target.value)}
+                value={data.responsibility}
+                name="responsibility"
+                placeholder='DD/MM/YY'
+                title='Write Your Responsibility...'
+              />
+            </div>
+          </div>
+        </div>
+        <div className="add-remove-btn d-flex align-items-center justify-content-between mb-50">
+          <div className="add-row">
+            <button type="button" className="add-experiences-row">Add New+ </button>
+          </div>
+        </div>
+        <div className="col-md-12">
+          <div className="form-inner">
+            <button className="primry-btn-2 lg-btn w-unset" type="submit">Update Change</button>
+          </div>
+        </div>
+      </form >
+    </div >
+  )
+}
+
+export default EditPersonalInfo
