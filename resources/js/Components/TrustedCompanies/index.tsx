75bdc6bc98b218b74b00823fb82e910c72e6269c
maximusbro <shidlovskiy.maksimka@gmail.com>
2023-06-11 16:12:02 -0600

    TrustedCompanies is done, need working slider

---
+++ resources/js/Components/TrustedCompanies/index.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+
+const TrustedCompanies = () => {
+  return (
+    <div className="home1-trusted-company two mb-120">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12">
+            <div className="section-title">
+              <h5>Our Trusted Company</h5>
+            </div>
+            <div className="swiper trusted-company-slider">
+              <div className="swiper-wrapper">
+                <div className="swiper-slide">
+                  <div className="company-logo">
+                    <img src="assets/images/bg/company-logo/trusted-company-01.png" alt="" />
+                  </div>
+                </div>
+                <div className="swiper-slide">
+                  <div className="company-logo">
+                    <img src="assets/images/bg/company-logo/trusted-company-02.png" alt="" />
+                  </div>
+                </div>
+                <div className="swiper-slide">
+                  <div className="company-logo">
+                    <img src="assets/images/bg/company-logo/trusted-company-03.png" alt="" />
+                  </div>
+                </div>
+                <div className="swiper-slide">
+                  <div className="company-logo">
+                    <img src="assets/images/bg/company-logo/trusted-company-04.png" alt="" />
+                  </div>
+                </div>
+                <div className="swiper-slide">
+                  <div className="company-logo">
+                    <img src="assets/images/bg/company-logo/trusted-company-05.png" alt="" />
+                  </div>
+                </div>
+                <div className="swiper-slide">
+                  <div className="company-logo">
+                    <img src="assets/images/bg/company-logo/trusted-company-06.png" alt="" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default TrustedCompanies
