2cff896e7ab5845f8c96b0ea110367a83cd5e847
maximusbro <shidlovskiy.maksimka@gmail.com>
2023-06-13 18:54:34 -0600

    Added LoggedUser + add Links in Header

--- resources/js/Components/Header/HeaderLogo/index.tsx
+++ resources/js/Components/Header/HeaderLogo/index.tsx
@@ -1,10 +1,11 @@
+import { Link } from "@inertiajs/react"
 import HeaderImg from "/public/assets/images/header1-logo.svg"
 const HeaderLogo = () => {
-	return (
-		<div className="header-main-logo d-lg-block d-none">
-			<a href="index.html"><img alt="image" src={HeaderImg} /></a>
-		</div>
-	)
+  return (
+    <div className="header-main-logo d-lg-block d-none">
+      <Link href="/"><img alt="image" src={HeaderImg} /></Link>
+    </div>
+  )
 }
 
 export default HeaderLogo
