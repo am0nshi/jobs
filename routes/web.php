4e6ccbc1e84af9a2dfa66496dda03af68e04fe44
mykytahalaiev <mykytahalaiev@cloud.upwork.com>
2023-06-14 20:37:39 -0600

    useBreakpoints hook

--- routes/web.php
+++ routes/web.php
@@ -35,53 +35,7 @@ Route::get('/', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard'); */
 
-Route::get('/my-profile', function () {
-    return Inertia::render('Profile/MyProfile');
-});
 
-Route::get('/applied-jobs', function () {
-    return Inertia::render('Profile/AppliedJobs');
-});
 
-Route::get('/settings', function () {
-    return Inertia::render('Profile/Settings');
-});
-
-Route::get('/view-resume', function () {
-    return Inertia::render('Profile/ViewResume');
-});
-Route::get('/bookmark', function () {
-    return Inertia::render('Profile/Bookmark');
-});
-
-Route::get('/dashboard', function () {
-    return Inertia::render('Profile/Dashboard');
-});
-
-/* EditResume routes */
-
-Route::get('/edit-resume', function () {
-    return Inertia::render('Profile/EditResume/BasicInformation');
-});
-
-Route::get('/edit-resume/profile', function () {
-    return Inertia::render('Profile/EditResume/Profile');
-});
-
-Route::get('/edit-resume/education', function () {
-    return Inertia::render('Profile/EditResume/Education');
-});
-
-Route::get('/edit-resume/professional-info', function () {
-    return Inertia::render('Profile/EditResume/ProfessionalInfo');
-});
-
-/* EditResume routes END */
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
+require __DIR__.'/web/profile.php';
 require __DIR__.'/auth.php';
