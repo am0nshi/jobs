b432c0276fe5557c970dfef8aea37dc64722bd34
alexanderyar <103504393+alexanderyar@users.noreply.github.com>
2023-06-19 16:37:21 -0600

    ds-store, custom.css

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,60 +1,67 @@
 import "../css/all.css";
-import "../css/app.css"
-import "../css/animate.css"
-import "../css/bootstrap.min.css"
-import './bootstrap';
+import "../css/app.css";
+import "../css/animate.css";
+import "../css/bootstrap.min.css";
+import "./bootstrap";
 
-import "../css/summernote.min.css"
-import "../css/boxicons.min.css"
-import "../css/bootstrap-icons.css"
-import "../css/jquery-ui.css"
-import "../css/swiper-bundle.css"
-import "../css/slick-theme.css"
-import "../css/slick.css";
-import "../css/nice-select.css"
-import "../css/select2.min.css"
-import "../css/jquery.fancybox.min.css"
-import "../css/datepicker.min.css"
+import "../css/summernote.min.css";
+import "../css/boxicons.min.css";
+import "../css/bootstrap-icons.css";
 import "../css/jquery-ui.css";
-import "../css/uiicss.css"
+import "../css/swiper-bundle.css";
+import "../css/slick-theme.css";
+import "../css/slick.css";
+import "../css/nice-select.css";
+import "../css/select2.min.css";
+import "../css/jquery.fancybox.min.css";
+import "../css/datepicker.min.css";
+import "../css/jquery-ui.css";
+import "../css/uiicss.css";
 import "../css/style.css";
+import "../css/custom.css";
 
-import { createRoot } from 'react-dom/client';
-import { createInertiaApp } from '@inertiajs/react';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import React from "react"
+import { createRoot } from "react-dom/client";
+import { createInertiaApp } from "@inertiajs/react";
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+import React from "react";
 import Layout from "@/Components/Layout";
-const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
+const appName =
+    window.document.getElementsByTagName("title")[0]?.innerText || "Laravel";
 
 createInertiaApp({
-  title: (title) => `${title} - ${appName}`,
+    title: (title) => `${title} - ${appName}`,
 
-  resolve: name => {
-    const page = resolvePageComponent(
-      `./Pages/${name}.tsx`,
-      import.meta.glob('./Pages/**/*.tsx')
-    );
-    page.then((module: any) => {
-      if ([
-        'Auth/Login',
-        'Auth/RegisterUser',
-        'Auth/RegisterCompany',
-      ].indexOf(name) === -1) {
-        module.default.layout = module.default.layout || (module => <Layout>{module}</Layout>);
-      }
-    });
-    return page
-  },
-  // resolve: (name) => resolvePageComponent(`./Pages/${name}.tsx`, import.meta.glob('./Pages/**/*.tsx')),
-  setup({ el, App, props }) {
-    const root = createRoot(el);
+    resolve: (name) => {
+        const page = resolvePageComponent(
+            `./Pages/${name}.tsx`,
+            import.meta.glob("./Pages/**/*.tsx")
+        );
+        page.then((module: any) => {
+            if (
+                [
+                    "Auth/Login",
+                    "Auth/RegisterUser",
+                    "Auth/RegisterCompany",
+                ].indexOf(name) === -1
+            ) {
+                module.default.layout =
+                    module.default.layout ||
+                    ((module) => <Layout>{module}</Layout>);
+            }
+        });
+        return page;
+    },
+    // resolve: (name) => resolvePageComponent(`./Pages/${name}.tsx`, import.meta.glob('./Pages/**/*.tsx')),
+    setup({ el, App, props }) {
+        const root = createRoot(el);
 
-    root.render(
-      <React.StrictMode>
-        <App {...props} />
-      </React.StrictMode>);
-  },
-  progress: {
-    color: '#4B5563',
-  },
+        root.render(
+            <React.StrictMode>
+                <App {...props} />
+            </React.StrictMode>
+        );
+    },
+    progress: {
+        color: "#4B5563",
+    },
 });
