ba3ed62279e5d0159b4d82db5a89aa5d8c903e41
Maksym Shydlovskyi <shidlovskiy.maksimka@gmail.com>
2023-06-30 16:16:26 -0600

    ResumeList Column card + fixes Expand

--- resources/js/Components/Expand/index.tsx
+++ resources/js/Components/Expand/index.tsx
@@ -10,7 +10,7 @@ const Expand = memo(({ children }: ExpandProps) => {
   };
 
   return (
-    <div className=' container mt-20'>
+    <>
       {/* block */}
       <div className='block'>
         {/* wrap */}
@@ -18,20 +18,13 @@ const Expand = memo(({ children }: ExpandProps) => {
         ${isOpened ? "max-h-[unset]" : "max-h-24"}
         overflow-hidden
         relative expand 
-        after:content-[""]
-        after:absolute 
-        after:bottom-0 
-        after:left-0 
-        after:w-full 
-        after:h-[50px] 
-        after:bg-white/[0%] 
-        after:bg-gradient-to-b 
-        after:from-[#F8F8F8]/[0%] 
-        after:to-[#F8F8F8]/[100%] 
-        after:border-0
+       ${!isOpened ? ' after:content-[""] after:absolute after:bottom-0 after:left-0 after:w-full after:h-[50px] after:bg-white/[0%] after:bg-gradient-to-b after:from-[#F8F8F8]/[0%] after:to-[#F8F8F8]/[100%] after:border-0'
+      :
+      ""
+      }
         `}>
           {/* Inner */}
-          <div className=' text-sm leading-6 text-[#999999] relative indent-[4em]'>
+          <div className=' text-sm leading-6 text-[#999999] relative '>
             {children}
           </div>
         </div>
@@ -45,7 +38,7 @@ const Expand = memo(({ children }: ExpandProps) => {
         cursor-pointer
         font-medium'>{isOpened ? "Close" : "Open"}</div>
       </div>
-    </div>
+  </>
   )
 }
 )
--- resources/js/Components/ResumeList/List/Column/ColumnCard/index.tsx
+++ resources/js/Components/ResumeList/List/Column/ColumnCard/index.tsx
@@ -2,55 +2,36 @@ import { Card } from 'primereact/card';
 import { Avatar } from "primereact/avatar";
 import { Button } from 'primereact/button';
 import React from 'react'
+import Expand from '@/Components/Expand';
 
 const ColumnCard = () => {
   const titleCard = (
 
     <div className='flex gap-2'>
-      <div>
-        <Avatar image="/images/avatar/amyelsner.png" size="xlarge" shape="circle" />
-      </div>
       <div className=''>
-        <b>Name</b>
-        <p>Position</p></div>
+        <b>Name N.</b>
+        </div>
 
     </div>
   )
 
   const footer = (
     <div className="flex flex-wrap justify-content-end gap-2">
-      <Button label="Save" icon="pi pi-check" />
-      <Button label="Cancel" icon="pi pi-times" className="p-button-outlined p-button-secondary" />
+      <Button label="Download CV" icon="pi pi-download" />
+     
     </div>
   );
   return (
     <Card title={titleCard} footer={footer}>
       <div>
-        <ul className='flex gap-10 list-none'>
-          <li className='relative before:content-[""] 
-          before:h-[9px]
-          before:w-[9px]
-          before:rounded-full
-          before:border-solid
-          before:border-2
-          before:border-[#00a7ac]
-          before:absolute
-            before:left-[-15px]
-          before:top-[7px] '>Expirience: 1 Year</li>
-          <li className=' relative before:content-[""] 
-          before:h-[9px]
-          before:w-[9px]
-          before:rounded-full
-          before:border-solid
-          before:border-2
-          before:border-[#00a7ac]
-          before:absolute
-          before:left-[-15px]
-          before:top-[7px] '>Salary: 100$</li>
-        </ul></div>
+       
+        </div>
       <div>
+        <Expand>
         <p>☛☛☛Do you want to have a professional, secured and fast solution, then you are in the right place.</p>
         <p>I create PSR-2 compatible, clear, intuitive, easily maintained, secure code. I know what is SOLID, DRY, KISS means and like to use them. Had expirience with relational databases over 500Gb, have deep understanding how they works and how to cook them. Familiar with security and potential attacks vectors. Familiar with async coding via queues/db/redis, daemons, resultset caching. Prefer to work with laravel projects.</p>
+        </Expand>
+      
       </div>
 
     </Card>
